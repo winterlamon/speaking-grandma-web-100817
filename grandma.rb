acc82308f5beb1a8c6b1ab700b46640a57953263
Winter LaMon <winter.lamon@gmail.com>
2017-09-20 23:48:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase.upcase = false
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
